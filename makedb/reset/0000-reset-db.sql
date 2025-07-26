dfcce4632079241feb846bbd8b1c56f42e222a59
Casey McQuillan <Casey@quilltechnologies.com>
2025-07-26 12:52:58 -0700

    Update db reset to account for new objects

--- makedb/reset/0000-reset-db.sql
+++ makedb/reset/0000-reset-db.sql
@@ -3,6 +3,12 @@ BEGIN
 
 DROP PROCEDURE IF EXISTS public.set_button_color;
 
+DROP PROCEDURE IF EXISTS public.update_button_stats;
+
+DROP TABLE IF EXISTS public.button_event;
+
+DROP TABLE IF EXISTS public.button_stat;
+
 DROP TABLE IF EXISTS public.button;
 
 DROP DOMAIN IF EXISTS fixed_bytea;
