4c6b67c0ee2d115e08bc5387d781cb65fbf1d386
Casey McQuillan <Casey@quilltechnologies.com>
2025-07-26 23:03:57 +0000

    Address bugs in stats db scripts

--- makedb/migrations/0004-create-stats-table.sql
+++ makedb/migrations/0004-create-stats-table.sql
@@ -1,3 +1,6 @@
+DO $$ 
+BEGIN
+
 CREATE TABLE IF NOT EXISTS button_event (
     id SERIAL PRIMARY KEY,
     x_coord INTEGER NOT NULL,
@@ -5,7 +8,7 @@ CREATE TABLE IF NOT EXISTS button_event (
     button_id INTEGER NOT NULL,
     event_type VARCHAR(32) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+);
 
 CREATE INDEX IF NOT EXISTS idx_button_event_event_type 
     ON button_event (event_type, created_at); 
@@ -21,13 +24,11 @@ CREATE TABLE IF NOT EXISTS button_stat (
 
 INSERT INTO button_stat (stat_key, stat_name, stat_desc, val, "scale", "order") 
 VALUES 
-('buttons_pressed', 'Buttons Pressed', 'Total number of buttons that users have pressed', 0, 1, 1),
-('pressed_last_day', 'Buttons pressed in the last day', 'Total number of buttons that users have pressed in the last 24 hours', 0, 1, 2),
+('buttons_pressed', 'Buttons Pressed', 'Total number of buttons that users have pressed', 0, 0, 1),
+('pressed_last_day', 'Buttons pressed in the last day', 'Total number of buttons that users have pressed in the last 24 hours', 0, 0, 2),
 ('presses_per_second', 'Presses per second', 'Average number of button presses per second', 0, -3, 3)
 ON CONFLICT (stat_key) DO NOTHING;
 
-DO $$
-BEGIN
 DROP PROCEDURE IF EXISTS update_button_stats;
 
 CREATE OR REPLACE PROCEDURE update_button_stats()
